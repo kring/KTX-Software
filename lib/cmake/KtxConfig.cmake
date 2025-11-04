887591197155a5623a16ce604ac5238d8650c3ec
Kevin Ring <kevin@kotachrome.com>
2025-11-04 20:04:31 +1100

    Make KtxConfig find the zstd dependency.

--- lib/cmake/KtxConfig.cmake
+++ lib/cmake/KtxConfig.cmake
@@ -1,7 +1,9 @@
 # Copyright 2015-2020 The Khronos Group Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-# include(CMakeFindDependencyMacro)
-# find_dependency()
+if(NOT TARGET zstd::libzstd)
+    include(CMakeFindDependencyMacro)
+    find_dependency(zstd CONFIG)
+endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/KtxTargets.cmake")
